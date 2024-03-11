57bf10eccc1e20fbe4206f5f65b1270abd044e7f
Xliang Liang <xliangliang@Xliangs-MBP.lan>
2024-03-12 01:15:15 +0800

    add upload.html

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
-#Prerequisite
+# 简介
+本项目搭建一个saas应用，为科研医生提供透明的fMRI图片结节标注数据分析。避免了学习晦涩难懂的python语言，以及环境搭建，fMRI图片处理lib。将大大提升医疗行业的效率。
+
+
+# Demo展示
+
+
+# 更新日志
+2024-3-12： 搭建python flask web框架，以及简单的图片上传分析页面。
+2024-3-15:  TODO 补充 分析fMRI图片结节代码，并且展示
+
+
+# 快速开始
+ ## 本地部署
+ ### 1.git 安装 & 拉取代码到本地
+   git clone git@github.com:xiangwuliang/fmri-analysis.git
+   cd fmri-analysis
+ ### 2.安装python & 核心依赖
+   pip3 install -r requirements.txt
+ ### 3.本地运行
+   flask run
+ ### 4.浏览器访问
+    http://127.0.0.1:5000/upload
+ ### 5.上传fMRI图片点击分析
+  
+## 生产部署 TODO
+
+### 购买阿里云
+### github action 自动部署
 
 
 
-# How to Build the project & run on local
 
--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import render_template, request, Flask
+import logging
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "hello world, python!"
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+## use this page to uplad fmri
+@app.route('/upload')
+def template():
+    return render_template('upload.html')
+
+## submit the fmri, and render the analysis result
+@app.route('/fmri/analysis', methods= ['POST'])
+def analysis():
+    f = request.files["file"]
+    file_name = f.filename
+    f.save(f"temporary/{file_name}")
+    ## TODO 拿到前段页面上传的文件以后，调用分析 fmri 图片，获取spacing数据，渲染页面
+    return "长:300mm, 宽40mm, 高:50mm, 坐标:44,66"
